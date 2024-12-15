7a38bbefc9e5895019cd6e36c26de4428e4e453d
ShinpeiH <dctechsf@gmail.com>
2024-12-14 22:26:59 -0800

    add many files

--- practice.js
+++ practice.js
@@ -554,3 +554,16 @@
 
 // factorial(3)
 
+// codewars
+
+// arr = [9, 3, '7', '3'];
+
+// function sumMix(x) {
+//     let result = 0;
+//     x.reduce((sum, num) => {
+//         return result += Number(num);
+//     }, 0);
+//     return result;
+// }
+
+// console.log(sumMix(arr));
